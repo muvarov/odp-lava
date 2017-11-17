116745de4b2d5051042cf7d6cfb9079920aa849e
Maxim Uvarov <maxim.uvarov@linaro.org>
2017-11-17 22:46:36 +0300

    initial commit
    Signed-off-by: Maxim Uvarov <maxim.uvarov@linaro.org>

---
+++ generator/client/generator-dpdk-client.sh
@@ -0,0 +1,120 @@
+#!/bin/bash
+set -o errexit
+
+# Install dir
+BUILD_DIR=${BUILD_DIR:-/root}
+DPDK_INSTALL_DIR=${DPDK_INSTALL_DIR, ${BUILD_DIR}/dpdk/x86_64-native-linuxapp-gcc}
+ODP_INSTALL_DIR=${ODP_INSTALL_DIR:-${BUILD_DIR}/install_dir}
+RUN_DIR=${RUN_DIR:-.}
+
+# name of the VLAND used
+VLAND_NAME=${VLAND_NAME:-vlan_one}
+
+# IP addresses
+LOCAL_IP=${LOCAL_IP:-"192.168.100.2"}
+REMOTE_IP=${REMOTE_IP:-"192.168.100.1"}
+
+# UDP ports
+LOCAL_PORT=${LOCAL_PORT:-5001}
+REMOTE_PORT=${REMOTE_PORT:-5000}
+
+# Cores mask
+CORES_MASK=${CORES_MASK:-"0x8"}
+
+# Packet Count
+PACKET_CNT=${PACKET_CNT:-600000000}
+
+# Echo parameters
+echo "BUILD_DIR = ${BUILD_DIR}"
+echo "ODP_INSTALL_DIR = ${ODP_INSTALL_DIR}"
+echo "DPDK_INSTALL_DIR = ${DPDK_INSTALL_DIR}"
+echo "RUN_DIR = ${RUN_DIR}"
+
+echo "VLAND_NAME = ${VLAND_NAME}"
+
+echo "LOCAL_IP = ${LOCAL_IP}"
+echo "REMOTE_IP = ${REMOTE_IP}"
+
+echo "LOCAL_PORT = ${LOCAL_PORT}"
+echo "REMOTE_PORT = ${REMOTE_PORT}"
+
+echo "CORES_MASK = ${CORES_MASK}"
+
+echo "PACKET_CNT= ${PACKET_CNT}"
+
+function what_vland_entry {
+        lava-vland-names | grep "^$1" | cut -d , -f 2
+}
+
+function what_vland_sys_path {
+        lava-vland-self | grep "$(what_vland_entry $1)" | cut -d , -f 3
+}
+
+function what_vland_MAC {
+        lava-vland-self | grep "$(what_vland_entry $1)" | cut -d , -f 2
+}
+
+# what_vland_interface
+# returns ethX assigned to VLAND_NAME
+function what_vland_interface {
+	        ls $(what_vland_sys_path $1)
+}
+
+if ! which lava-wait &>/dev/null; then
+        echo "This script must be executed in LAVA"
+        exit
+fi
+
+# Setup DPDK
+sysctl -w vm.nr_hugepages=1024
+sh -c 'echo 1024 > /sys/devices/system/node/node0/hugepages/hugepages-2048kB/nr_hugepages' || true
+mkdir ${RUN_DIR}/huge || true
+mount -t hugetlbfs nodev ${RUN_DIR}/huge || true
+
+depmod
+modprobe uio
+insmod ${DPDK_INSTALL_DIR}/kmod/igb_uio.ko || true
+
+# Setup DPDK interface
+dev=$(what_vland_interface ${VLAND_NAME})
+echo "dev = ${dev}"
+
+DEV_PCI=`${BUILD_DIR}/dpdk/usertools/dpdk-devbind.py --status | grep $dev | awk '{print $1}'`
+echo "DEV_PCI = ${DEV_PCI}"
+
+LOCAL_MAC=$(what_vland_MAC ${VLAND_NAME})
+echo "LOCAL_MAC = ${LOCAL_MAC}"
+
+${BUILD_DIR}/dpdk/usertools/dpdk-devbind.py -u ${DEV_PCI}
+${BUILD_DIR}/dpdk/usertools/dpdk-devbind.py --bind=igb_uio ${DEV_PCI}
+${BUILD_DIR}/dpdk/usertools/dpdk-devbind.py -s
+
+echo ">> SEND client_ready"
+lava-send client_ready
+
+echo "<< Wait server_ready"
+lava-wait server_ready
+REMOTE_MAC=$(cat /tmp/lava_multi_node_cache.txt | cut -d = -f 2)
+echo "REMOTE_MAC = ${REMOTE_MAC}"
+
+sleep 2
+echo "Test start..."
+
+taskset 0xff ${ODP_INSTALL_DIR}/bin/odp_generator -I 0 --srcmac ${LOCAL_MAC} --dstmac ${REMOTE_MAC} \
+                --srcip ${LOCAL_IP} --dstip ${REMOTE_IP} -m u -i 0 -c ${CORES_MASK} -p 18 \
+                -e ${LOCAL_PORT} -f ${REMOTE_PORT} -n ${PACKET_CNT} > /tmp/generator_client.data
+
+echo "TX:"
+sync || true
+cat /tmp/generator_client.data
+
+MAX_SEND_RATE=`cat /tmp/generator_client.data | grep "max send rate:" | tail -n 1 | awk '{print $12}'`
+echo "MAX_SEND_RATE = ${MAX_SEND_RATE}"
+
+echo ">> SEND client_done"
+lava-send client_done
+
+echo "<< Wait server_done"
+lava-wait server_done
+
+echo "A10"
---
+++ generator/server/odp_generator_recv_dpdk.sh
@@ -0,0 +1,102 @@
+#!/bin/bash
+set -o errexit
+
+# Install dir
+BUILD_DIR=${BUILD_DIR:-/root}
+echo "BUILD_DIR = ${BUILD_DIR}"
+ODP_INSTALL_DIR=${ODP_INSTALL_DIR:-${BUILD_DIR}/install_dir}
+echo "ODP_INSTALL_DIR = ${ODP_INSTALL_DIR}"
+DPDK_INSTALL_DIR=${DPDK_INSTALL_DIR, ${BUILD_DIR}/dpdk/x86_64-native-linuxapp-gcc}
+echo "DPDK_INSTALL_DIR = ${DPDK_INSTALL_DIR}"
+RUN_DIR=${RUN_DIR:-.}
+echo "RUN_DIR = ${RUN_DIR}"
+
+# name of the VLAND used
+VLAND_NAME=${VLAND_NAME:-vlan_one}
+echo "VLAND_NAME = ${VLAND_NAME}"
+
+# Cores mask
+CORES_MASK=${CORES_MASK:-0x4}
+echo "CORES_MASK = ${CORES_MASK}"
+
+function exit_error {
+	echo "-- SERVER ERROR"
+	lava-test-case server_up --status fail
+}
+trap exit_error ERR
+
+function what_vland_entry {
+	lava-vland-names | grep "^$1" | cut -d , -f 2
+}
+
+function what_vland_sys_path {
+	lava-vland-self | grep "$(what_vland_entry $1)" | cut -d , -f 3
+}
+
+function what_vland_MAC {
+        lava-vland-self | grep "$(what_vland_entry $1)" | cut -d , -f 2
+}
+
+# what_vland_interface
+# returns ethX assigned to VLAND_NAME
+function what_vland_interface {
+	ls $(what_vland_sys_path $1)
+}
+
+if ! which lava-wait &>/dev/null; then
+	echo "This script must be executed in LAVA"
+	exit
+fi
+
+# Setup DPDK
+sysctl -w vm.nr_hugepages=1024
+sh -c 'echo 1024 > /sys/devices/system/node/node0/hugepages/hugepages-2048kB/nr_hugepages' || true
+mkdir ${RUN_DIR}/huge || true
+mount -t hugetlbfs nodev ${RUN_DIR}/huge || true
+
+depmod
+modprobe uio
+insmod ${DPDK_INSTALL_DIR}/kmod/igb_uio.ko || true
+
+# Setup DPDK interface
+dev=$(what_vland_interface ${VLAND_NAME})
+echo "dev = ${dev}"
+
+DEV_PCI=`${BUILD_DIR}/dpdk/usertools/dpdk-devbind.py --status | grep $dev | awk '{print $1}'`
+echo "DEV_PCI = ${DEV_PCI}"
+
+LOCAL_MAC=$(what_vland_MAC ${VLAND_NAME})
+echo "LOCAL_MAC = ${LOCAL_MAC}"
+
+${BUILD_DIR}/dpdk/usertools/dpdk-devbind.py -u ${DEV_PCI}
+${BUILD_DIR}/dpdk/usertools/dpdk-devbind.py --bind=igb_uio ${DEV_PCI}
+${BUILD_DIR}/dpdk/usertools/dpdk-devbind.py -s
+
+echo "<< WAIT client_ready"
+lava-wait client_ready
+
+echo "Test start..."
+cd ${RUN_DIR}
+taskset 0xff ${ODP_INSTALL_DIR}/bin/odp_generator -I 0 -m r -c ${CORES_MASK} > /tmp/app.data &
+echo $! > /tmp/app.pid
+
+echo ">> SEND server_ready"
+lava-send server_ready peer_mac=${LOCAL_MAC}
+
+echo "<< WAIT client_done"
+lava-wait client_done
+
+sync || true
+kill -9 `cat /tmp/app.pid`
+
+cat /tmp/app.data
+
+RESULT=`cat /tmp/app.data | tail -n 1  | grep "sent:"`
+RESULT_RATE=`echo $RESULT | awk '{print $23}'`
+RESULT_UNIT=`echo $RESULT | awk '{print $24}'`
+
+lava-test-case RX_rate --result pass --measurement $RESULT_RATE --units $RESULT_UNIT
+
+echo ">> SEND server_done"
+lava-send server_done
+echo "A10"
